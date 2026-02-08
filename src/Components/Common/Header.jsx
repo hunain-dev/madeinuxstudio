f121f1c8924c41a221cccc6d052c1b769575d077
huniiali1122@gmail.com <huniiali1122@gmail.com>
2026-02-09 03:07:58 +0500

    updated

--- src/Components/Common/Header.jsx
+++ src/Components/Common/Header.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='h-[20vh] w-full fixed z-999'>
+    <div className='h-[10vh] flex items-center justify-between bg-blue-300 w-full fixed z-999'>
+      
       
     </div>
   )
--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,56 +1,67 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import Image from "./Image";
+import Logo from "../../public/Assets/Images/logo.png";
+import gsap from "gsap";
 
 const Loader = () => {
-    const obj = [
-        {
-            border:"border-l-1 border-black"
-        },  {
-            border:"border-l-1 border-black"
-        },
-        {
-            border:"border-l-1 border-black"
-        },  {
-            border:"border-l-1 border-black"
-        },  {
-            border:"border-l-1 border-black"
-        },  {
-            border:"border-l-1 border-black"
-        },  {
-            border:"border-l-1 border-black"
-        },  {
-            border:"border-l-1 border-black"
-        },
-        {
-            border:"border-l-1 border-black"
-        },
-        {
-            border:"border-l-1 border-black"
-        },
-        {
-            border:"border-l-1 border-black"
-        },
-    ]
-  return (
-    <div className='h-screen w-full opacity-100 bg-[#B9BDAB] fixed z-99999 '>
+  const columnsRef = useRef([]);
+  const logoRef = useRef(null);
+
+  const obj = Array(11).fill("#B9BDAB"); // 11 columns
+
+  useEffect(() => {
+    // Columns all together
+    gsap.fromTo(
+      columnsRef.current,
+      { x: "-100%" }, // start off-screen left
+      {
+        x: "2%",       // final position
+        duration: 1,   // 1 second
+        ease: "power2.out",
+        marker:true
+      }
+    );
   
-                    <div  className="back h-full w-full  relative grid grid-cols-11" >
-                        {
-                            obj.map((elem,index)=>{
-                                return(
-                                    <div key={index}   className=" border-l border-gray-500"
-                                    style={{backgroundColor:elem.backgroundcolor,border:elem.border}}></div>
+    // Logo fade-in after columns
+    gsap.fromTo(
+      logoRef.current,
+      { opacity: 0, scale: 0.8 },
+      {
+        opacity: 1,
+        scale: 1,
+        duration: 0.8,
+        delay: 1, // wait 1s for columns
+        ease: "power2.out",
+      }
+    );
 
-                                )
-                            })
-                        }
-            
-                </div>
 
-    <div className="front absolute top-0 opacity-100 left-0 bg-blue-500 h-full w-full z-99"></div>
+  }, []);
+  
 
-      
+  return (
+    <div className="h-screen w-full fixed z-50 bg-[#B9BDAB] overflow-hidden">
+      {/* Columns */}
+      <div className="back h-full w-full grid grid-cols-11 relative">
+        {obj.map((bg, index) => (
+          <div
+            key={index}
+            ref={(el) => (columnsRef.current[index] = el)}
+            className="h-full w-full"
+            style={{ backgroundColor: bg }}
+          ></div>
+        ))}
+      </div>
+
+      {/* Logo */}
+      <div
+        ref={logoRef}
+        className="front absolute top-0 left-0 h-full w-full flex items-center justify-center"
+      >
+        <Image src={Logo} className="h-80 w-80 object-contain" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
